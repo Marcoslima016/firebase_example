f9d39fe80cadf351a644a6fb71578bc36f2561b4
Marcos V. R. Lima <marcosvr.lima96@gmail.com>
2022-03-25 17:35:46 -0300

    update

--- lib/ler_valores.dart
+++ lib/ler_valores.dart
@@ -3,6 +3,14 @@ import 'package:firebase_database/firebase_database.dart';
 class LerValores {
   Future lerTodosValoresDeUmPath() async {
     final response = await FirebaseDatabase.instance.reference().child("clientes").once();
+
+    //
+    response.value.forEach((key, values) {
+      var t1 = key;
+      var t2 = values;
+    });
+
+    //
     var point = "";
   }
 }
