c7b469be366035b20c5033931cefd4e0fb141c5a
Marcos V. R. Lima <marcosvr.lima96@gmail.com>
2022-03-28 15:33:31 -0300

    update

--- lib/ler_valores.dart
+++ lib/ler_valores.dart
@@ -1,16 +1,23 @@
 import 'package:firebase_database/firebase_database.dart';
 
 class LerValores {
-  Future lerTodosValoresDeUmPath() async {
-    final response = await FirebaseDatabase.instance.reference().child("clientes").once();
+  //
 
+  //Metodo que consulta valores salvos na "tabela" clientes
+  Future lerTodosValoresDeUmPath() async {
     //
+    //Referencia do banco de dados
+    DatabaseReference database = FirebaseDatabase.instance.reference();
+
+    // Nessa linha é feita a leitura dos dados que estão dentro do path "clientes"
+    final response = await database.child("clientes").once();
+
+    // Nessas proximas linhas são percorridos os valores retornados na consulta feita na linha 8. Ou seja, é percorrido cada cliente retornado na consulta
     response.value.forEach((key, values) {
-      var t1 = key;
-      var t2 = values;
+      var chave = key; //// Nesse caso, cada chave representa um cliente salvo na "tabela" clientes.
+      var valores = values; //// Valores salvos dentro do cliente atual
     });
 
     //
-    var point = "";
   }
 }
